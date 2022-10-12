9d2974d0e1d17b3bb3e43432a39620c4d96ebd6e
HappyKawara <c1139136@planet.kanazawa-it.ac.jp>
2022-10-12 19:05:08 +0900

    first commit

---
+++ src/regular_expression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import pprint
+import os
+
+file_path = os.path.expanduser("~/ダウンロード/")
+
+with open(file_path + "ダウンロード/301_ruby_5915","r") as f:
+    for line in f:
+
